80e3dd275d521d4450e9f817fbab2c40b9c395d9
Sumon Datta <sumon.datta@outlook.com>
2017-10-19 09:41:00 -0500

    analysis of elf dna

---
+++ houseelf-analysis.R
@@ -0,0 +1,11 @@
+#Author: Sumon Datta
+#Date: 10-19-2017
+#Assignment 7 version control basics
+
+#part 1
+#setup Git
+
+#Part 2
+#This analysis is intended to categorize the ear length and get the type of sequences 
+#of each id and categorize them by length
+
