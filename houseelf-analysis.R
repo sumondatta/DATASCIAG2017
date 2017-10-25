9395104f0058d6d6a12358d33d0e9493cfe3f45b
Sumon Datta <sumon.datta@outlook.com>
2017-10-24 23:22:19 -0500

    Added GC content functions

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -2,10 +2,34 @@
 #Date: 10-19-2017
 #Assignment 7 version control basics
 
-#part 1
+#part 1: Setup Git
 #setup Git
 
-#Part 2
+#part 2: First commit
 #This analysis is intended to categorize the ear length and get the type of sequences 
 #of each id and categorize them by length
+
+#part 3: importing data
+
+elf<-read.csv("./data/houseelf_earlength_dna_data_1.csv", header = TRUE, sep = ",")
+#the file above was also comitted to version control
+
+#part 4
+
+#data file was renamed to _1 and committed to version control
+
+#part 5 adding a remote
+#created account in github and then created public repository and integrated the repository with 
+#desktop version through adding command lines in shell
+
+#part6: pushing changes
+library (stringr)
+gc_cont <- function (seq){
+  Gs <- str_count (str_to_upper(seq), "G")
+  Cs <- str_count(str_to_upper(seq), "C")
+  gc_content <- (Gs + Cs) / (str_length(seq)) * 100
+  return (gc_content)
+}
+
+print (gc_content <- gc_cont (elf$dnaseq))
 
