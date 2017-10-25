636240de49a636cb4ad63d192d4e2d3e49f261dd
Phillip D. Alderman <phillip.alderman@okstate.edu>
2017-10-25 10:16:30 -0500

    Adding function to classify ear lengths

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -9,7 +9,6 @@
 #This analysis is intended to categorize the ear length and get the type of sequences 
 #of each id and categorize them by length
 
-<<<<<<< HEAD
 #part 3: importing data
 
 elf<-read.csv("./data/houseelf_earlength_dna_data_1.csv", header = TRUE, sep = ",")
@@ -46,5 +45,11 @@ print (ear_class <- get_size_class(elf$earlength))
 print (combined_data <- data.frame(elf$id, ear_class, gc_content))
 
 write.csv (combined_data, "./elf-data_final.csv")
+
+get_size_class <- function(ear_length){
+   # Calculate the size class for one or more earth lengths
+   ear_lengths <- ifelse(ear_length > 10, "large", "small")
+   return(ear_lengths)
+}
 
 #END OF CODE
